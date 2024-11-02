8f7dc6a72b3c364dfc26792641102f7a548bf686
Evgeny <113383200+KlassnayaAfrodita@users.noreply.github.com>
2024-11-02 23:48:52 +0300

    Update workerPool.go

--- workerPool.go
+++ workerPool.go
@@ -119,4 +119,11 @@ func main() {
 	manager := NewManager(2, 10) // Минимум 2 воркера, максимум 10
 
 	// Добавляем задачи с задержкой
-	for i := 0; i <
+	for i := 0; i < 20; i++ {
+		manager.AddTask(fmt.Sprintf("Task %d", i))
+		time.Sleep(200 * time.Millisecond)
+	}
+
+	manager.Stop() // Останавливаем все воркеры и завершаем работу
+	fmt.Println("All tasks completed, exiting program.")
+}
