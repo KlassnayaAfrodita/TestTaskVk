3d6a9db410037c8572593dd4738dccb871894543
Evgeny <113383200+KlassnayaAfrodita@users.noreply.github.com>
2024-11-03 19:17:13 +0300

    Update worker_pool_test.go

--- internal/workerpool/worker_pool_test.go
+++ internal/workerpool/worker_pool_test.go
@@ -11,23 +11,23 @@ import (
 
 // Тест минимального количества воркеров
 func TestMinWorkers(t *testing.T) {
-	manager := NewManager(2, 5)
-	manager.Start()
+	workerPool := NewWorkerPool(2, 5)
+	workerPool.Start()
 
-	if atomic.LoadInt32(&manager.workerCount) != 2 {
-		t.Errorf("Expected 2 workers at start, got %d", manager.workerCount)
+	if atomic.LoadInt32(&workerPool.workerCount) != 2 {
+		t.Errorf("Expected 2 workers at start, got %d", workerPool.workerCount)
 	}
 
-	manager.Stop()
+	workerPool.Stop()
 }
 
 // Тест максимального количества воркеров при высокой нагрузке
 func TestMaxWorkers(t *testing.T) {
-	manager := NewManager(2, 5)
-	manager.Start()
+	workerPool := NewWorkerPool(2, 5)
+	workerPool.Start()
 
 	for i := 0; i < 100; i++ {
-		manager.AddTask(fmt.Sprintf("Task %d", i))
+		workerPool.AddTask(fmt.Sprintf("Task %d", i))
 	}
 
 	ticker := time.NewTicker(100 * time.Millisecond)
@@ -36,12 +36,12 @@ func TestMaxWorkers(t *testing.T) {
 	for {
 		select {
 		case <-time.After(3 * time.Second):
-			t.Errorf("Expected 5 workers, but got %d", atomic.LoadInt32(&manager.workerCount))
+			t.Errorf("Expected 5 workers, but got %d", atomic.LoadInt32(&workerPool.workerCount))
 			return
 		case <-ticker.C:
-			if atomic.LoadInt32(&manager.workerCount) == 5 {
+			if atomic.LoadInt32(&workerPool.workerCount) == 5 {
 				fmt.Println("Reached maximum number of workers: 5")
-				manager.Stop()
+				workerPool.Stop()
 				return
 			}
 		}
@@ -50,36 +50,36 @@ func TestMaxWorkers(t *testing.T) {
 
 // Тест на завершение всех задач
 func TestAllTasksCompleted(t *testing.T) {
-	manager := NewManager(2, 5)
-	manager.Start()
+	workerPool := NewWorkerPool(2, 5)
+	workerPool.Start()
 
 	for i := 0; i < 5; i++ {
-		manager.AddTask(fmt.Sprintf("Task %d", i))
+		workerPool.AddTask(fmt.Sprintf("Task %d", i))
 	}
 
 	time.Sleep(3 * time.Second)
 
-	if len(manager.taskQueue) != 0 {
-		t.Errorf("Expected all tasks to be completed, but %d tasks remain", len(manager.taskQueue))
+	if len(workerPool.taskQueue) != 0 {
+		t.Errorf("Expected all tasks to be completed, but %d tasks remain", len(workerPool.taskQueue))
 	}
 
-	manager.Stop()
+	workerPool.Stop()
 }
 
 // Тест автоматического уменьшения числа воркеров
 func TestAutoScaleDown(t *testing.T) {
-	manager := NewManager(2, 5)
-	manager.Start()
+	workerPool := NewWorkerPool(2, 5)
+	workerPool.Start()
 
 	for i := 0; i < 10; i++ {
-		manager.AddTask(fmt.Sprintf("Task %d", i))
+		workerPool.AddTask(fmt.Sprintf("Task %d", i))
 		time.Sleep(100 * time.Millisecond)
 	}
 
 	// ждем, пока задачи поступят и начнут исполнятся
 	time.Sleep(2 * time.Second)
 
-	initialWorkers := atomic.LoadInt32(&manager.workerCount)
+	initialWorkers := atomic.LoadInt32(&workerPool.workerCount)
 	if initialWorkers < 2 || initialWorkers > 5 {
 		t.Errorf("Unexpected worker count after scaling up: got %d, want between %d and %d", initialWorkers, 2, 5)
 	}
@@ -87,28 +87,28 @@ func TestAutoScaleDown(t *testing.T) {
 	time.Sleep(3 * time.Second)
 
 	// ждем, пока выполнятся задачи и воркеры удалятся
-	finalWorkers := atomic.LoadInt32(&manager.workerCount)
+	finalWorkers := atomic.LoadInt32(&workerPool.workerCount)
 	if finalWorkers != 2 {
 		t.Errorf("Expected worker count to scale down to 2, but got %d", finalWorkers)
 	}
 
-	manager.Stop()
+	workerPool.Stop()
 	fmt.Println("TestAutoScaleDown completed")
 }
 
 // Тест на остановку воркер пула
 func TestStopAllWorkers(t *testing.T) {
-	manager := NewManager(2, 5)
-	manager.Start()
+	workerPool := NewWorkerPool(2, 5)
+	workerPool.Start()
 
 	for i := 0; i < 5; i++ {
-		manager.AddTask(fmt.Sprintf("Task %d", i))
+		workerPool.AddTask(fmt.Sprintf("Task %d", i))
 	}
 
-	manager.Stop()
-	
+	workerPool.Stop()
+
 	// Проверяем, что все воркеры остановлены
-	activeWorkers := atomic.LoadInt32(&manager.workerCount)
+	activeWorkers := atomic.LoadInt32(&workerPool.workerCount)
 	if activeWorkers != 0 {
 		t.Errorf("Expected all workers to be stopped, but %d workers are still active", activeWorkers)
 	} else {
